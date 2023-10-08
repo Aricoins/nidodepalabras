d4794ae9a44b256c78ee0481008f75fe77873ddc
Aricoins <arielgarcia79@gmail.com>
2023-10-08 04:49:50 -0300

    2322

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
-import url from "../assets/bosque.gif";
+import url from "../assets/giphy.gif";
 import Icon from "./Icon";
 import styles from "./Head.css";
 
 const Imagen = styled.img`
-  width: 100%;
+  width: 1200px;
   height: 600px;
   background-color: #fe9a17;
 `;
 
 const flyAnimation = keyframes`
   0% {
-    transform: translate(-1000px, -100px) rotate(2deg) scale(0.3) ;
+    transform: translate(-1000px, -100px) rotate(2deg) scale(0.1) ;
   opacity: 1
   }
 
@@ -36,28 +36,28 @@ background-color: #e4c01c;
 border-radius: 100%;
 }
   65% {
-transform: translate(-200, 0px)rotate(-30deg) scaleX();
+transform: translate(-200, -100px)rotate(-30deg) scaleX();
 opacity: 1;
 background-color: #e4c01c;
 border-radius: 100%;
   }
     66% {
-transform: translate(-400, 0px)rotate(-30deg) scaleX();
+transform: translate(0px, 0px)rotate(-30deg) scaleX();
 opacity: 1;
 border-radius: 100%;
   }
    70% {
-transform: translate(-800, 0px)rotate(-30deg) scaleX();
+transform: translate(-200px, 0px)rotate(-30deg) scaleX();
 opacity: 1;
-background-color: rgba(255, 0, 0, 0); /* Establece el fondo inicialmente transparente */
 }
 
 75%{
-transform: translate(-800px, 0px)scale(2);
+transform: translate(-500px, 0px)scale(1.5);
 opacity:1;
+background-color: rgba(255, 0, 0, 0);  
 }
 100%{
-transform: translate(800px, -150px)scale(4);
+transform: translate(500px, -400px)scale(4);
 opacity:1;
 }
 `;
@@ -83,17 +83,26 @@ transition: traslate (0, 0);
   }
 
 `
-const Bird2 = styled.h1`
+const Titulo = styled.h1`
   position: absolute;
-  top: 40px;
-  right: -200px;
-  width: 900px;
-  color: #fafafe;
-  box-shadow:inset blak ; 
+  top: 300px;
+  left: 200px;
+   width: 900px;
+  color: #f8f9f8;
+  box-shadow:inset black ; 
  font-family: Verdana, Geneva, Tahoma, sans-serif;
-  font-style: oblique;
+  font-size: 80px;
   animation: ${fadeIn} 2s normal;
 `;
+const Subtitulo= styled.div`
+ position: absolute;
+ top: 450px;
+  left: 350px;
+   width: 1000px;
+  color: #02f302;
+  height: 200px;
+  font-size: 50px;
+opacity:0.7`
 
 export default function Head() {
   return (
@@ -103,9 +112,11 @@ export default function Head() {
       <Bird>
         <Icon className={styles.tamanio}></Icon>
             </Bird>
-            <Bird2>
-            <h1>Nido de palabras</h1>
-            </Bird2>
+            
+            <Titulo>Nido de Palabras</Titulo>
+            <Subtitulo>nidodepalabras.online</Subtitulo>
+           
+    
     </div>
   );
 }
